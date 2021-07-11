f20c92e9310dd75f11eaa9f5a3a8dd75b301c35b
earosselot <earosselot@gmail.com>
2021-07-11 20:37:57 -0300

    dom module project starting

---
+++ src/domManipulation.js
@@ -0,0 +1,13 @@
+//TODO: make DOM-module
+
+// function createBoard(boardId) {
+//     const board2 = document.getElementById(boardId);
+//     for (let i = 0; i < 100; i++) {
+//         let tile = document.createElement('div')
+//         tile.setAttribute('id', `${boardId}-${i}`)
+//         tile.classList.add('tile')
+//         boardContainer.appendChild(tile)
+//     }
+// }
+//
+// module.exports = createBoard
